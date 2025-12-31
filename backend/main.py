8a32d5f1b83cf9f3dcc57a818092aef2b1f54e5e
Aayushmaan <aayushmaan.chakraborty@gmail.com>
2025-12-31 13:48:12 +0530

    updated to use local LLM

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile , File
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, JSONResponse
+import whisper
+import ollama
+import os
+import shutil
 
 app = FastAPI(
     title = "AI Voice Transcriber",
@@ -11,6 +15,15 @@ app = FastAPI(
 # serve frontend files
 app.mount("/static", StaticFiles(directory="frontend"),name="static")
 
+# load whisper local model at startup
+print("Loading whisper base model ... Might take some time (very less) 🙂‍↕️")
+whisper_model = whisper.load_model('base')
+print("Whisper has been loaded (never doubt me) 🥰")
+
+# temporary recording
+TEMP_DIR = "../temp"
+os.makedirs(TEMP_DIR, exist_ok=True)
+
 @app.get("/", response_class = HTMLResponse)
 def home():
     return """
@@ -19,3 +32,41 @@ def home():
         <a href = "/static/index.html"> Click here if not redirected </a>
     </p>
     """
+
+@app.post("/transcribe")
+async def transcribe(audio: UploadFile=File(...)):
+    try:
+        # save audio temporarily
+        temp_path = os.path.join(TEMP_DIR, "temp_audio.webm")
+        with open(temp_path, "wb") as f:
+            shutil.copyfileobj(audio.file, f)
+        
+        # transcribe with whisper
+        result = whisper_model.transcribe(temp_path)
+        raw_transcript = result['text'].strip()
+
+        # clean up with local llm (llama3.2:3b)
+        prompt = f"""
+Clean up this spoken transcript. Remove filler words (um, uh, like, you know), repetitions, false starts, and fix grammar. Make it concise, natural, and readable while keeping the original meaning
+
+Raw transcript : 
+{raw_transcript}
+
+Clean transcript:
+"""
+        response = ollama.generate(model = 'llama3.2:3b', prompt = prompt)
+        clean_transcript = response['response'].strip()
+        
+        # clean the temp file
+        os.remove(temp_path)
+
+        return JSONResponse({
+            "transcript" : clean_transcript,
+            "raw" : raw_transcript
+        })
+    except Exception as e:
+        if os.path.exists(temp_path):
+            os.remove(temp_path)
+        return JSONResponse({
+            "error" : str(e)
+        }, status_code = 500)
